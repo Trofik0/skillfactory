3491d732e97ca4f45db94ecb7bfc301741a21b26
Trofik0 <veronichka_trofik@vk.com>
2025-02-25 20:55:45 +0400

    database.h

---
+++ database.h
@@ -0,0 +1,27 @@
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QVariant>
+#include <QStringList>
+
+class Database : public QObject {
+    Q_OBJECT
+
+public:
+    explicit Database(QObject *parent = nullptr);
+    bool connectToDatabase(const QString &host, const QString &dbName, const QString &user, const QString &password);
+    QStringList getMessages(); // Получить сообщения
+    QStringList getUsers();    // Получить пользователей
+    void banUser(int userId);
+    void disconnectUser(int userId);
+
+private:
+    QSqlDatabase db;
+};
+
+#endif // DATABASE_H
